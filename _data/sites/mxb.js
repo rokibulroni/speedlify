3e24d2ca8ca52a44e3e4443e48c3aa7c80099c49
Rokibul Islam Roni <checkoutrons@gmail.com>
2022-03-23 06:29:00 +0600

    Create mxb.js

---
+++ _data/sites/mxb.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'www.rokibulroni.com',
+    description: 'Personal website',
+    options: {
+        frequency: 1,
+        freshChrome: 'site'
+    },
+    urls: [
+        'https://www.rokibulroni.com/',
+        'https://www.rokibulroni.com/blog/',
+        'https://www.rokibulroni.com/notes/',
+        'https://www.rokibulroni.com/about/'
+    ],
+    skip: false
+}
